548c87dd8b36f9c3543a8289c4d076e713cac747
Alexandra Dernova <alexder@gmx.net>
2023-08-01 08:10:36 -0700

    link auf der homeseite

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,21 @@
 import Head from "next/head";
 import styled from "styled-components";
 import { Inter } from "next/font/google";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
+const NavLink = styled(Link)`
+text-decoration: none;
+color: white;
+border-color: black;
+align: center;
+&: hover {
+  font-size: 1.2em;}
+
+&: target {
+  color: red;
+  font-size: 1.2em;
+}`;
 
 export default function Home() {
   return (
@@ -16,6 +29,7 @@ export default function Home() {
       <main className={inter.className}>
         <Heading>🐧Penguin Capstone Template🐧</Heading>
       </main>
+      <NavLink id="Locations List" href="/locations">Zu der List von Locations</NavLink>
     </>
   );
 }

b15a03d0a9ffedf79df48b9b409b70c50921befc
Alexandra Dernova <alexder@gmx.net>
2023-08-02 09:16:50 -0700

    update

--- pages/api/locations/index.js
+++ pages/api/locations/index.js
@@ -5,7 +5,6 @@ export default async function handler(request, response) {
   await dbConnect();
   if (request.method === "GET") {
     const locations = await Location.find();
-    const data = response.status(200).json(locations);
-    return data;
+    response.status(200).json(locations);
   }
 }
